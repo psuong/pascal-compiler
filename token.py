a6fbf5029f128c47f711f7ead433a379b425fbad
Porrith Suong <psuong95@gmail.com>
2016-03-07 16:48:37 -0500

    Adds keywords to the repository and updates token class.

--- token.py
+++ token.py
@@ -1,18 +1,27 @@
 class Token:
     def __init__(self):
         self.TK_Operators = {
-            '(': TK_OPEN_PARENTH,
-            ')': TK_CLOSE_PARENTH,
-            '.': TK_DOT,
-            ';': TK_SEMI_COLON,
-            '+': TK_PLUS,
-            '-': TK_MINIS,
-            '/': TK_DIVIDE,
-            '*': TK_MULTIPLY,
-            '=': TK_EQUAL,
-            ',': TK_COMMA,
-            '>': TK_GREATER,
-            '<': TK_LESS
+            '(': 'TK_OPEN_PARENTH',
+            ')': 'TK_CLOSE_PARENTH',
+            '.': 'TK_DOT',
+            ';': 'TK_SEMI_COLON',
+            '+': 'TK_PLUS',
+            '-': 'TK_MINUS',
+            '/': 'TK_DIVIDE',
+            '*': 'TK_MULTIPLY',
+            '=': 'TK_EQUAL',
+            ',': 'TK_COMMA',
+            '>': 'TK_GREATER',
+            '<': 'TK_LESS',
+            ':=': 'TK_ASSIGNMENT'
         }
         self.TK_Characters = ['abcdefghijklmnoprqstuvwxyz']
         self.TK_Keywords = {}
+
+        # Set up the Token Operators
+        tk_keyword_setup(TK_Keywords)
+
+    def tk_keyword_setup(keyword_dictionary):
+        with open('keywords.txt', 'r') as keyword_file:
+            for line in keyword_file:
+                TK_Keywords[line] = 'TK_%s' % (line)
