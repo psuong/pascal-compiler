a926223147e42738502a4c5a0e3e439e91fdbc97
Porrith Suong <psuong95@gmail.com>
2016-03-13 13:23:49 -0400

    Adds memory read lines to scanner.

--- token-generator.py
+++ token-generator.py
@@ -2,12 +2,19 @@ from sys import argv
 from mmap import mmap
 
 
+character_list = 'abcdefghijklmnopqrstuvwxyz'
+
+
 def open_pascal_file():
 
     with open(argv[1], 'r+b') as pascal_file:
         # Map the entire file to memory
         mem_mapped_file = mmap(pascal_file.fileno(), 0)
-        print mem_mapped_file.readline()
+        word = ''
+        for line in mem_mapped_file:
+            for char in line:
+                if char in character_list:
+                    word += char
 
 
 if __name__ == '__main__':
