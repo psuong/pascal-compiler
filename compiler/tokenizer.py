75820d160e1148b99ab1d9edf27d58bcad0a9e65
Porrith Suong <psuong95@gmail.com>
2016-04-26 16:07:07 -0400

    Adds object orientated version of compiler.

--- compiler/tokenizer.py
+++ compiler/tokenizer.py
@@ -1,7 +1,10 @@
+from mmap import mmap
+from sys import argv
+
+
 class Token:
     def __init__(self):
-        # TODO: Add a <= and >= operator.
-        self.TK_Operators = {
+        self.TK_OPERATORS = {
             '(': 'TK_OPEN_PARENTH',
             ')': 'TK_CLOSE_PARENTH',
             '.': 'TK_DOT',
@@ -14,33 +17,50 @@ class Token:
             '=': 'TK_EQUAL',
             ',': 'TK_COMMA',
             '>': 'TK_GREATER',
+            '>=': 'TK_GREATER_EQ',
             '<': 'TK_LESS',
+            '<=': 'TK_LESS_EQ',
             ':=': 'TK_ASSIGNMENT'
         }
-        self.TK_Keywords = None
-        self.TK_File = {
-            'EOF': 'TK_EOF'
-        }
-        self.TK_Digit = 'TK_DIGIT'
-        self.TK_Datatypes= {
-            'string': 'TK_DATATYPE_STRING',
-            'real': 'TK_DATATYPE_REAL',
-            'integer': 'TK_DATATYPE_INTEGER',
-            'boolean': 'TK_DATATYPE_BOOLEAN',
-            'character': 'TK_DATATYPE_CHARACTER'
-        }
-        self.TK_declarations = {
 
+        self.TK_KEYWORDS = self.keyword_setup()
+        self.TK_FILE = 'TK_EOF'
+        self.TK_DIGIT = {
+            '.': 'TK_REAL_LITERAL',
+            'integer': 'TK_INTEGER_LITERAL'
+        }
+        self.TK_DATATYPES = {
+            'string': 'TK_STRING',
+            'real': 'TK_REAL',
+            'integer': 'TK_INTEGER',
+            'boolean': 'TK_BOOLEAN',
+            'char': 'TK_CHARACTER'
         }
 
-    @property
-    def tk_keyword_setup(self):
-        """
-        Generates a dictionary of keywords found in Pascal.
-        :return: dictionary
-        """
+        self.TK_VAR = 'TK_A_VAR'
+        self.TK_STRING = 'TK_STRING_LIT'
+        self.TK_ID = 'TK_IDENTIFIER'
+
+    @staticmethod
+    def keyword_setup():
         keyword_dict = {}
         with open('compiler/keywords.txt', 'r') as keyword_file:
             for line in keyword_file:
-                keyword_dict[line.upper().rstrip()] = 'TK_KEYWORD_%s' % (line.upper().rstrip())
+                keyword_dict[line.rstrip()] = 'TK_KEYWORD_%s' % (line.upper())
         return keyword_dict
+
+
+class FileManager:
+    def __init__(self):
+        self.pascal_file = self.open_pascal_file()
+
+    def open_pascal_file(self):
+        with open(argv[1], 'r + b') as pascal_file:
+            memory_mapped_file = mmap(pascal_file.fileno(), 0)
+        return memory_mapped_file
+
+
+class Scanner:
+    def __init__(self):
+        memory_mapped_file = None
+    pass
