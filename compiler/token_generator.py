56e4d1f81a2eec3e8567d0ba0e269ec450d28e0c
Porrith Suong <psuong95@gmail.com>
2016-05-14 13:45:44 -0400

    Updates master.

--- compiler/token_generator.py
+++ compiler/token_generator.py
@@ -1,26 +1,4 @@
 from tokenizer import Token, FileManager, Scanner
-from sys import argv
-            word += char
-            if not char.isdigit() and word.isdigit():
-                assign_token_values(token.TK_Digit['integer'],
-                                    word,
-                                    col_num,
-                                    line_num,
-                                    True)
-                word = ''
-                current_state = scanner_state.DIGIT_CASE
-            elif not char.isdigit() and '.' in word:
-                assign_token_values(token.TK_Digit['.'],
-                                    word,
-                                    col_num,
-                                    line_num,
-                                    True)
-                word = ''
-                current_state = scanner_state.DIGIT_CASE
-            elif char.isalpha():
-                word = char
-                current_state = scanner_state.NORMAL_CASE
-
 
 def load_pascal_file():
     pascal_file = FileManager()
