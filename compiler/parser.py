61c59f5b506b865bb73add34b5192efa71858f42
Porrith Suong <psuong95@gmail.com>
2016-05-16 14:17:15 -0400

    Adds member variables to parser object.

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,10 +1,8 @@
-from tokenizer import Token
-
-
-class Parser(Token):
-    """
-    A -> Token Value
-    Var -> Token Var
-    """
-    def __init__(self, token):
-        pass
+class Parser(object):
+    def __init__(self, token_list):
+        self.token_list = token_list
+        self.current_token = None
+        self.instruction_pointer = 0
+        self.data_pointer = 0
+        self.symbol_table = []
+        self.byte_array = bytearray(1000) # For now allocate a large amount of memory
