70648cea6955c553b4960780c93a4b1f373d7e93
Porrith Suong <psuong95@gmail.com>
2016-05-16 14:58:31 -0400

    Adds generate_opcode function, match, and lookup function.

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,3 +1,7 @@
+class Error(Exception):
+    pass
+
+
 class Parser(object):
     def __init__(self, token_list):
         self.token_list = token_list
@@ -5,4 +9,44 @@ class Parser(object):
         self.instruction_pointer = 0
         self.data_pointer = 0
         self.symbol_table = []
-        self.byte_array = bytearray(1000) # For now allocate a large amount of memory
+        self.byte_array = bytearray(1000)  # For now allocate a large amount of memory
+
+    def find_symbol_table_entry(self, name):
+        """
+        Looks through the entries in the symbol table and checks for the name. If it
+        exists, return the instance of the symbol. Otherwise, return nothing.
+        :param name: string
+        :return: Symbol
+        """
+        for symbol in self.symbol_table:
+            if symbol.name == name:
+                return symbol
+        return None
+
+    def match_token(self, token_type):
+        """
+        Matches the current token with the token type.
+        :param token_type: string
+        :return: None
+        """
+        if self.current_token.object_type == token_type:
+            print 'Matched: %s, %s ' % (token_type, self.current_token.object_type)
+            try:
+                # Get the next token list if available
+                self.current_token = self.token_list.next()
+            except StopIteration:
+                return None
+        else:
+            raise Error('Token not found:\n Received: %s \n Got: %s' % (
+                str(token_type),
+                str(self.current_token.object_type)
+            ))
+
+    def generate_opcode(self, op_code):
+        """
+        Creates op_code and assigns it to the byte array. The instruction_pointer is
+        additionally incremented by one.
+        :return: None
+        """
+        self.byte_array[self.instruction_pointer] = op_code
+        self.instruction_pointer += 1
